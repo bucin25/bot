d398f2816d9b3ebb215a0bdc05997f79ddeb0cb9
jingga <76574693+bucin25@users.noreply.github.com>
2021-09-04 19:08:38 +0700

    Update App.php

--- application/controllers/App.php
+++ application/controllers/App.php
@@ -184,11 +184,11 @@ class App extends CI_Controller {
     }
     $this->climate->out(" {$this->cyan}¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤");
     $this->climate->out(" {$this->cyan}  [¤ {$this->reset}RC{$this->cyan} ¤] {$this->reset}      REMOVE COOKIE       {$this->cyan}[¤ {$this->reset}RC{$this->cyan} ¤]");
-    $this->climate->out(" {$this->cyan}  [¤ {$this->reset}RB{$this->cyan} ¤] {$this->reset}REPORT BUG                {$this->cyan}[¤ {$this->reset}RC{$this->cyan} ¤]");
-    $this->climate->out(" {$this->cyan}  [¤ {$this->reset}UT{$this->cyan} ¤] {$this->reset}UPDATE TOOLS              {$this->cyan}[¤ {$this->reset}RC{$this->cyan} ¤]");
+    $this->climate->out(" {$this->cyan}  [¤ {$this->reset}RB{$this->cyan} ¤] {$this->reset}       REPORT BUG         {$this->cyan}[¤ {$this->reset}RC{$this->cyan} ¤]");
+    $this->climate->out(" {$this->cyan}  [¤ {$this->reset}UT{$this->cyan} ¤] {$this->reset}      UPDATE TOOLS        {$this->cyan}[¤ {$this->reset}RC{$this->cyan} ¤]");
     $this->climate->out(" {$this->cyan}  [¤ {$this->reset}SC{$this->cyan} ¤] {$this->reset}SEE YOUR FACEBOOK COOKIES {$this->cyan}[¤ {$this->reset}RC{$this->cyan} ¤]");
-    $this->climate->out(" {$this->cyan}  [¤ {$this->reset}AT{$this->cyan} ¤] {$this->reset}ABOUT THIS TOOLS          {$this->cyan}[¤ {$this->reset}RC{$this->cyan} ¤]");
-    $this->climate->out(" {$this->cyan}  [¤ {$this->reset}EX{$this->cyan} ¤] {$this->reset}EXIT                      {$this->cyan}[¤ {$this->reset}RC{$this->cyan} ¤]");
+    $this->climate->out(" {$this->cyan}  [¤ {$this->reset}AT{$this->cyan} ¤] {$this->reset     ABOUT THIS TOOLS      {$this->cyan}[¤ {$this->reset}RC{$this->cyan} ¤]");
+    $this->climate->out(" {$this->cyan}  [¤ {$this->reset}EX{$this->cyan} ¤] {$this->reset}          EXIT            {$this->cyan}[¤ {$this->reset}RC{$this->cyan} ¤]");
     $this->climate->out(" {$this->cyan}¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤");
     $this->climate->out("");
     $this->climate->out("");
