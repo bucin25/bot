073af59b38b8bcf1e441602f8ff6ed65e81c7fce
jingga <76574693+bucin25@users.noreply.github.com>
2021-09-04 18:04:19 +0700

    Update App.php

--- application/controllers/App.php
+++ application/controllers/App.php
@@ -182,14 +182,12 @@ class App extends CI_Controller {
     {
       $this->climate->info("     New version is available. update your fb-bot now, type 'ut' for update");
     }
-    $this->climate->out('  ¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤');
     $this->climate->out("    [{$this->cyan}RC{$this->reset}]. Remove Cookies");
     $this->climate->out("    [{$this->cyan}RB{$this->reset}]. Report Bug");
     $this->climate->out("    [{$this->cyan}UT{$this->reset}]. Update Tools");
     $this->climate->out("    [{$this->cyan}SC{$this->reset}]. See Your FB Cookies");
     $this->climate->out("    [{$this->cyan}AT{$this->reset}]. About This Tools");
     $this->climate->out("    [{$this->cyan}EX{$this->reset}]. Exit");
-    $this->climate->out('  ¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤');
     foreach ($this->configs->show_menu() as $menu)
     {
       $this->climate->out("    [{$this->yellow}{$menu->no}{$this->reset}]. $menu->name");
