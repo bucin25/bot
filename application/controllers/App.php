188a4c5eca4319f068410fd84e2c8521a3a5064f
jingga <76574693+bucin25@users.noreply.github.com>
2021-09-05 10:02:17 +0700

    Update App.php

--- application/controllers/App.php
+++ application/controllers/App.php
@@ -220,7 +220,7 @@ class App extends CI_Controller {
         $this->about_tools();
         break;
       case '01':
-        $this->tools->chat messages eraser($title);
+        $this->tools->chat_messages_eraser($title);
         break;
       case '02':
         $this->tools->post_eraser($title);
